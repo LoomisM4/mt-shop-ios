61cf4a092f42e3adc553c23bd5e5dec97c6cd571
LoomisM4 <mail@marcel-wettach.com>
2022-03-07 15:57:45 +0100

    Initial Commit

---
+++ mt-shop-ios/mt_shop_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mt_shop_iosApp.swift
+//  mt-shop-ios
+//
+//  Created by Marcel Wettach on 07.03.22.
+//
+
+import SwiftUI
+
+@main
+struct mt_shop_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
